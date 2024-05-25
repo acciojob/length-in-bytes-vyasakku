f40b126f0c289f369ee43b1e4f222e8b5ab69c2c
vyasakku <120413112+vyasakku@users.noreply.github.com>
2024-05-25 17:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const byteSize = (str) => {
   // write your code here
+	return str.length;
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
